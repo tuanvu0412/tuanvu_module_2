17c68097a02ec9163a3fbe351406f76784321af9
tuanvu041299 <tuanvu041299@gmail.com>
2023-05-05 20:09:28 +0700

    Case Study

--- src/case_study/furama_resort/service/booking/BookingService.java
+++ src/case_study/furama_resort/service/booking/BookingService.java
@@ -1,25 +1,19 @@
 package case_study.furama_resort.service.booking;
 
 import case_study.furama_resort.model.Booking;
-import case_study.furama_resort.model.faciliti.Facility;
-import case_study.furama_resort.model.person.Customer;
 import case_study.furama_resort.repository.booking.BookingRepositoryImpl;
 import case_study.furama_resort.repository.customer.CustomerRepositoryImpl;
 import case_study.furama_resort.repository.facility.FacilityRepositoryImpl;
-import case_study.furama_resort.utils.booking.ReadAndWriteToBooking;
 import case_study.furama_resort.utils.booking.ValidateBooking;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class BookingService implements IBookingService {
     Scanner sc = new Scanner(System.in);
-    private BookingRepositoryImpl bookingRepository = new BookingRepositoryImpl();
-    private CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl();
-    private FacilityRepositoryImpl facilityRepository = new FacilityRepositoryImpl();
-    private TreeSet<Booking> list = bookingRepository.getList();
+    private final BookingRepositoryImpl bookingRepository = new BookingRepositoryImpl();
+    private final CustomerRepositoryImpl customerRepository = new CustomerRepositoryImpl();
+    private final FacilityRepositoryImpl facilityRepository = new FacilityRepositoryImpl();
+    private final TreeSet<Booking> list = bookingRepository.getList();
 
     public void display() {
         for (Booking b:list ) {
@@ -29,62 +23,63 @@ public class BookingService implements IBookingService {
 
     @Override
     public void add() {
-        System.out.println("nhâp id booking");
+        System.out.print("nhâp id booking: ");
         String idBooking = sc.nextLine();
         String date;
         do {
-            System.out.println("nhập ngày(dd-mm-yyyy)");
+            System.out.print("nhập ngày(dd-mm-yyyy): ");
             date = sc.nextLine();
             if (ValidateBooking.checkDate(date)) {
-                System.out.println("thêm thành công");
+                System.out.println("thêm thành công.");
             } else {
-                System.out.println("nhập khống đúng định dạng");
+                System.out.println("nhập khống đúng định dạng.");
             }
         } while (!ValidateBooking.checkDate(date));
         String dateStart;
         do {
-            System.out.println("nhập ngày bắt đầu booking(dd-mm-yyyy):");
+            System.out.print("nhập ngày bắt đầu booking(dd-mm-yyyy): ");
             dateStart = sc.nextLine();
             if (ValidateBooking.checkDate(dateStart)) {
-                System.out.println("nhập thành công");
+                System.out.println("nhập thành công.");
             } else {
-                System.out.println("nhập không đúng định dạng");
+                System.out.println("nhập không đúng định dạng.");
             }
         } while (!ValidateBooking.checkDate(dateStart));
         String dateEnd;
         do {
-            System.out.println("nhập ngày kết thúc booking(dd-mm-yyyy):");
+            System.out.print("nhập ngày kết thúc booking(dd-mm-yyyy): ");
             dateEnd = sc.nextLine();
             if (ValidateBooking.checkDate(dateEnd)) {
-                System.out.println("nhập thành công");
+                System.out.println("nhập thành công.");
             } else {
-                System.out.println("nhập không đúng định dạng");
+                System.out.println("nhập không đúng định dạng.");
             }
         } while (!ValidateBooking.checkDate(dateEnd));
         String idCustomer;
         do {
-            System.out.println("nhập id của khách");
+            System.out.print("nhập id của khách: ");
             idCustomer = sc.nextLine();
             if (customerRepository.findId(idCustomer) == -1) {
-                System.err.println("id không tìm thấy");
+                System.err.println("id không tìm thấy.");
                 continue;
             } else {
-                System.out.println("thêm id thành công");
+                System.out.println("thêm id thành công.");
             }
             break;
         } while (true);
         String idService;
         do {
-            System.out.println("nhập id dịch vụ");
+            System.out.print("nhập id dịch vụ: ");
             idService = sc.nextLine();
             if (facilityRepository.finId(idService) == -1) {
-                System.out.println("không tìm thấy id");
+                System.out.println("không tìm thấy id.");
                 continue;
             } else {
-                System.out.println("thêm thành công");
+                System.out.println("thêm thành công.");
             }
             break;
         } while (true);
         bookingRepository.addNew(new Booking(idBooking, date, dateStart, dateEnd, idCustomer, idService));
+        System.out.println("Đã thêm thành công booking!!!");
     }
 }
--- src/case_study/furama_resort/service/contract/ContactService.java
+++ src/case_study/furama_resort/service/contract/ContactService.java
@@ -22,61 +22,62 @@ public class ContactService implements IContactService {
 
     @Override
     public void add() {
-        System.out.println("nhập số hợp đồng");
+        System.out.print("nhập số hợp đồng: ");
         String contractNumber = sc.nextLine();
         String idBooking;
         do {
-            System.out.println("nhập id booking");
+            System.out.print("nhập id booking: ");
             idBooking = sc.nextLine();
             if (bookingRepository.finIdBooking(idBooking).equals("")) {
-                System.out.println("không tìm thấy id");
+                System.out.println("không tìm thấy id.");
                 continue;
             } else {
-                System.out.println("thêm thành công");
+                System.out.println("thêm thành công.");
 
             }
             break;
         } while (true);
-        System.out.println("nhập số tiền cọc trước");
+        System.out.print("nhập số tiền cọc trước: ");
         String depositInAdvance = sc.nextLine();
-        System.out.println("nhập tổng số tiền phải thanh toán");
+        System.out.print("nhập tổng số tiền phải thanh toán: ");
         String totalPaymentAmount = sc.nextLine();
         contactRepository.add(new Contract(contractNumber, idBooking, depositInAdvance, totalPaymentAmount));
+        System.out.println("đã thêm thành công!!!");
     }
 
     @Override
     public void editContact() {
-        System.out.println("nhập số contact mà bạn muốn sửa");
+        System.out.print("nhập số hợp đồng mà bạn muốn sửa: ");
         String oldContactNumber = sc.nextLine();
         int edit = contactRepository.checkId(oldContactNumber);
         Contract contract = contactRepository.getContactList().get(edit);
         if (contract == null) {
-            System.out.println("không tìm thấy id");
+            System.out.println("không tìm thấy id.");
         } else {
-            System.out.print("nhập số contact mới");
+            System.out.print("nhập số hợp đồng mới.");
             String newContactNumber = sc.nextLine();
             contract.setIdBooking(newContactNumber);
             String newIdBooking;
             do {
-                System.out.println("nhập id booking");
+                System.out.print("nhập id booking: ");
                 newIdBooking = sc.nextLine();
                 if (bookingRepository.finIdBooking(newIdBooking).equals("")) {
-                    System.out.println("không tìm thấy id");
+                    System.out.println("không tìm thấy id.");
                     continue;
                 } else {
-                    System.out.println("thêm thành công");
+                    System.out.println("thêm thành công.");
 
                 }
                 break;
             } while (true);
             contract.setIdBooking(newIdBooking);
-            System.out.print("nhập lại số tiền đã cọc trước");
+            System.out.print("nhập lại số tiền đã cọc trước: ");
             String newDepositInAdvance = sc.nextLine();
             contract.setIdBooking(newDepositInAdvance);
-            System.out.print("nhập lại tổng số tiền phải trả");
+            System.out.print("nhập lại tổng số tiền phải trả: ");
             String newTotalPaymentAmount = sc.nextLine();
             contract.setIdBooking(newTotalPaymentAmount);
-            System.out.println("đã được thay đổi");
+            System.out.println("hợp đồng đã được thay đổi.");
         }
     }
 }
